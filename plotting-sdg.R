e5b7a5fdf65f80dce4cf6df28d3c57d14eacfbd0
jramonlab <jrhg82@hotmail.com>
2020-04-26 21:05:55 +0200

    Andalucía and Granada DONE

--- plotting-sdg.R
+++ plotting-sdg.R
@@ -42,7 +42,6 @@ sdg2018es %>%
 sdg2018es %>%   
   ggplot(aes(x = reorder(.$'Region name', desc(region_rank)), sdg_avg)) +
   geom_boxplot() + 
-  geom_point() + 
   coord_flip() +
   scale_x_discrete(labels = c("Andalucía" = expression(bold("*ANDALUCÍA")), parse=TRUE)) + 
   ggtitle("Distribución Índice ODS de las ciudades por CCAA. 2018") + 
@@ -59,8 +58,65 @@ sdg2018es %>%
 ggsave("figs/ods_2018_cities_distribution.png", width = 15, height = 10)
 
 
-
+##
+# Convert to tidy format
+library(tidyr)
+sdg2018es_tidy <- sdg2018es %>% 
+  gather(key = SDG_NUM, value = SDG_VAL, 
+                                sdg01, sdg02, sdg03, sdg04, sdg05, sdg06, sdg07, sdg08,
+                                sdg09, sdg10, sdg11, sdg12, sdg13, sdg14, sdg15, sdg16, sdg17)
 
 #
 # SDGs index across different SDGs in ANDALUCIA
+sdg2018es_tidy %>% 
+  filter(region_id == "ES-AN") %>%
+  ggplot(aes(x = reorder(SDG_NUM, SDG_VAL), SDG_VAL)) +
+  geom_boxplot() + 
+  geom_point(alpha = 0.2) + 
+  coord_flip()
+
+
+sdg2018es_tidy %>% 
+  filter(region_id == "ES-AN") %>%
+  ggplot(aes(x = reorder(SDG_NUM, SDG_VAL), SDG_VAL)) +
+  geom_boxplot() +
+  geom_point(size = 3, aes(color = (City == "Granada"), alpha = (City == "Granada"))) +
+  coord_flip() + 
+  ggtitle("Distribución Índice ODS ANDALUCÍA. 2018") + 
+  xlab("") + 
+  ylab("Índice ODS. (0-100)") +
+  annotate(geom="text", x=2.5, y=86, label = "Fuente: REDS.SDSN 2018",color="darkred", size = 4.5)+
+  annotate(geom="text", x=1.5, y=84, label = "Gráficos: JRLAB. 2020",color="darkblue", size = 4.5) +
+  theme_bw() +
+  theme(title = element_text(size = 20),
+        axis.title = element_text(size = 15),
+        axis.text = element_text(size = 15),
+        aspect.ratio = 1/1)
+
+
+
+#
+# SDGs index across different SDGs in GRANADA
+
+sdg2018es_tidy %>% 
+  filter(City == "Granada") %>%
+  ggplot(aes(x = reorder(SDG_NUM, SDG_VAL),
+             y = SDG_VAL,
+             label = round(SDG_VAL, 1))) +
+  geom_text(nudge_y = 5, color = "black", size = 5) +
+  geom_col(alpha = 0.5, fill = "lightblue") +
+  coord_flip() +
+  scale_x_discrete(labels = c("Andalucía" = expression(bold("*ANDALUCÍA")), parse=TRUE)) + 
+  scale_y_continuous(limits = c(0,100)) +
+  ggtitle("Índice ODS GRANADA.2018") + 
+  xlab("") + 
+  ylab("Índice ODS. (0-100)") +
+  annotate(geom="text", x=2.5, y=86, label = "Fuente: REDS.SDSN 2018",color="darkred", size = 4.5)+
+  annotate(geom="text", x=1.5, y=84, label = "Gráficos: JRLAB. 2020",color="darkblue", size = 4.5) +
+  theme_classic() +
+  theme(title = element_text(size = 20),
+        axis.title = element_text(size = 15),
+        axis.text = element_text(size = 15),
+        aspect.ratio = 1/1)
+
 
